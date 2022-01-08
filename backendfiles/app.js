f7efc4f951325b38b74cc92df39b470c5ca2404e
Juan Jose Duque Cardona <juanjo.dc@outlook.com>
2022-01-07 19:54:06 -0500

    apks ordenados y express configurado para la descarga de los recursos

--- backendfiles/app.js
+++ backendfiles/app.js
@@ -7,15 +7,18 @@ const port=3000
 app.get('/marcadores/:nombre',(req,res)=>{ 
     const marcador=req.params.nombre;
     var archivo="";  
-
     archivo=archivo.concat(__dirname,"/marcadores","/",marcador,".jpg"); 
-    
-
-    //const file=`${__dirname}/marcadores/biomas.jpg`; 
-    res.download(archivo);
+    res.download();
 
 })
 
+app.get('/apk/:nombre',(req,res)=>{ 
+    const marcador=req.params.nombre;
+    var archivo="";  
+    archivo=archivo.concat(__dirname,"/apk","/",marcador,".jpg"); 
+    res.download();
+
+})
 
 
 app.listen(port,()=>{
